ae13375b79f272af131157b7813678d8b4b8cb82
Igor <igor.torquato.20@gmail.com>
2023-03-07 20:28:12 -0300

    att

--- 01_Exercício_Semana/resolucao.py
+++ 01_Exercício_Semana/resolucao.py
@@ -1,17 +1,78 @@
 def euler():
-    eps = 0.001
-    t = 2.0
-    i = 2
-    
-    
-    while True:
-        tprev = t
-        
-        t = pow(1.0 + (1.0 / i), i);
-        
-        if (fabs(tprev - t) < eps):
-             break
-        i=i+1
-    
+    n = 100000000
+    euler = (1 + 1/n) ** n
+    return euler
 
-euler()
+print(euler())
+
+''''
+Another way to calculate the value of "e" in Python is to use the fact that "e" is the limit of the expression:
+
+bash
+
+(1 + 1/n)^n
+as n approaches infinity. We can use this fact to calculate an approximation of "e" by choosing a large value of n and computing the expression:
+
+python
+
+def calculate_euler():
+    n = 1000000
+    euler = (1 + 1/n) ** n
+    return euler
+In this code, we are choosing n to be a large number (in this case, 1 million), and computing the expression (1 + 1/n)^n to approximate the value of "e". This method can be less computationally intensive than the previous method, as it only requires one calculation instead of a loop. However, the accuracy of this method depends on the value of n chosen, and a larger value of n may be required to obtain a more accurate approximation.
+
+
+
+
+
+import math
+
+def calculate_euler():
+    e = 1
+    factorial = 1
+    n = 1
+    while True:
+        factorial *= n
+        term = 1/factorial
+        e += term
+        n += 1
+        if math.isclose(term, 0, rel_tol=1e-16):
+            break
+    return e
+
+euler = calculate_euler()
+print(euler)
+
+
+
+To calculate the Euler's number "e" in Python without using the Sympy library, you can use the following code:
+
+python
+
+import math
+
+def calculate_euler():
+    e = 1
+    factorial = 1
+    n = 1
+    while True:
+        factorial *= n
+        term = 1/factorial
+        e += term
+        n += 1
+        if math.isclose(term, 0, rel_tol=1e-16):
+            break
+    return e
+
+euler = calculate_euler()
+print(euler)
+In this code, we are using the Taylor series to approximate the value of "e". The Taylor series for "e" is:
+
+makefile
+
+e = 1 + 1/1! + 1/2! + 1/3! + ...
+We can calculate the value of "e" by computing the terms in the series until the difference between two consecutive terms is very small (i.e., less than a certain tolerance level). In this code, we are using a relative tolerance of 1e-16 to determine when to stop calculating terms.
+
+Note that the math.isclose() function is used to compare the difference between two consecutive terms to zero. This is because comparing floating-point numbers for equality can be problematic due to rounding errors. The math.isclose() function takes into account the magnitude of the numbers being compared, so it is a more reliable way to check if two floating-point numbers are close to each other.
+
+'''
